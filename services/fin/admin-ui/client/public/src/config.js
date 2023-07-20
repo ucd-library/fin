aa7871a4068eb9db3a19bd794e7c7b868ae5406e
Justin Merz <jrmerz@gmail.com>
2023-07-20 11:56:33 -0700

    quick fix for dams admin ui

--- services/fin/admin-ui/client/public/src/config.js
+++ services/fin/admin-ui/client/public/src/config.js
@@ -3,6 +3,6 @@ const config = APP_CONFIG || {};
 config.env.REPO_URL = 'https://github.com/ucd-library/fin';
 config.repoUrl = config.env.REPO_URL;
 config.baseVersionUrl = config.repoUrl + '/tree/'+ config.env.FIN_REPO_TAG;
-config.baseDocsUrl = baseVersionUrl + '/docs';
+config.baseDocsUrl = config.baseVersionUrl + '/docs';
 
 export default config
