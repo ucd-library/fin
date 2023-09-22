4d94cee61bc1b60e6a608d70fd711fe7ffbc9d19
Justin Merz <jrmerz@gmail.com>
2023-09-22 13:36:47 -0700

    fix for docker buildx cache

--- devops/build.sh
+++ devops/build.sh
@@ -13,6 +13,23 @@ else
   FIN_SERVER_REPO_HASH=$SHORT_SHA
 fi
 
+DOCKER_BUILD="docker buildx build"
+
+# for google cloud multi-arch builds
+if [[ $LOCAL_DEV == 'true' ]]; then
+  DOCKER_BUILD="$DOCKER_BUILD --pull --output=type=docker"
+else
+  echo "Setting up docker buildx using arm machine: $ARM64_MACHINE_IP"
+  docker context create amd_node --docker "host=unix:///var/run/docker.sock"
+  docker context create arm_node --docker "host=ssh://ci-bot@$ARM64_MACHINE_IP"
+
+  docker buildx create --use --name ucd-lib-builder --platform linux/amd64 amd_node
+  docker buildx create --append --name ucd-lib-builder --platform linux/arm64 arm_node
+
+  DOCKER_BUILD="$DOCKER_BUILD --platform linux/amd64,linux/arm64 --push --pull"
+fi
+
+
 echo "Fin Repository:"
 echo "Branch: $FIN_BRANCH_NAME"
 echo "Tag: $FIN_TAG_NAME"
@@ -24,96 +41,106 @@ echo    "Postgres      : $POSTGRES_IMAGE_NAME:$APP_TAG and :$DOCKER_CACHE_TAG"
 echo    "Apache LB     : $LB_IMAGE_NAME:$APP_TAG and :$DOCKER_CACHE_TAG"
 echo    "Base Service  : $SERVER_IMAGE_NAME:$APP_TAG and :$DOCKER_CACHE_TAG"
 echo    "ElasticSearch : $ELASTIC_SEARCH_IMAGE_NAME:$APP_TAG and :$DOCKER_CACHE_TAG"
-echo    "RabbitMQ : $ELASTIC_SEARCH_IMAGE_NAME:$APP_TAG and :$DOCKER_CACHE_TAG"
+echo    "RabbitMQ      : $ELASTIC_SEARCH_IMAGE_NAME:$APP_TAG and :$DOCKER_CACHE_TAG"
 echo    "PG Rest       : $PGREST_IMAGE_NAME:$APP_TAG and :$DOCKER_CACHE_TAG"
 echo -e "Init          : $INIT_IMAGE_NAME:$APP_TAG and :$DOCKER_CACHE_TAG\n"
 
+echo -e "Docker build command: $DOCKER_BUILD\n"
+
 # Core Server - fcrepo
-docker build \
+$DOCKER_BUILD \
   --build-arg FIN_APP_VERSION=${APP_VERSION} \
   --build-arg FIN_REPO_TAG=${FIN_TAG_NAME} \
   --build-arg FIN_BRANCH_NAME=${FIN_BRANCH_NAME} \
   --build-arg FIN_SERVER_REPO_HASH=${FIN_SERVER_REPO_HASH} \
   -t $FCREPO_IMAGE_NAME:$APP_TAG \
+  -t $FCREPO_IMAGE_NAME:$DOCKER_CACHE_TAG \
   --cache-from $FCREPO_IMAGE_NAME:$DOCKER_CACHE_TAG \
+  --cache-to=type=inline \
   services/fcrepo
-docker tag $FCREPO_IMAGE_NAME:$APP_TAG $FCREPO_IMAGE_NAME:$DOCKER_CACHE_TAG
 
 # Core Server - postgres
-docker build \
+$DOCKER_BUILD \
   --build-arg FIN_APP_VERSION=${APP_VERSION} \
   --build-arg FIN_REPO_TAG=${FIN_TAG_NAME} \
   --build-arg FIN_BRANCH_NAME=${FIN_BRANCH_NAME} \
   --build-arg FIN_SERVER_REPO_HASH=${FIN_SERVER_REPO_HASH} \
   -t $POSTGRES_IMAGE_NAME:$APP_TAG \
+  -t $POSTGRES_IMAGE_NAME:$DOCKER_CACHE_TAG \
   --cache-from $POSTGRES_IMAGE_NAME:$DOCKER_CACHE_TAG \
+  --cache-to=type=inline \
   services/postgres
-docker tag $POSTGRES_IMAGE_NAME:$APP_TAG $POSTGRES_IMAGE_NAME:$DOCKER_CACHE_TAG
 
 # Core Server - apache lb
-docker build \
+$DOCKER_BUILD \
   --build-arg FIN_APP_VERSION=${APP_VERSION} \
   --build-arg FIN_REPO_TAG=${FIN_TAG_NAME} \
   --build-arg FIN_BRANCH_NAME=${FIN_BRANCH_NAME} \
   --build-arg FIN_SERVER_REPO_HASH=${FIN_SERVER_REPO_HASH} \
   -t $LB_IMAGE_NAME:$APP_TAG \
+  -t $LB_IMAGE_NAME:$DOCKER_CACHE_TAG \
   --cache-from $LB_IMAGE_NAME:$DOCKER_CACHE_TAG \
+  --cache-to=type=inline \
   services/load-balancer
-docker tag $LB_IMAGE_NAME:$APP_TAG $LB_IMAGE_NAME:$DOCKER_CACHE_TAG
 
 # Core Server - server
-docker build \
+$DOCKER_BUILD \
   --build-arg FIN_APP_VERSION=${APP_VERSION} \
   --build-arg FIN_REPO_TAG=${FIN_TAG_NAME} \
   --build-arg FIN_BRANCH_NAME=${FIN_BRANCH_NAME} \
   --build-arg FIN_SERVER_REPO_HASH=${FIN_SERVER_REPO_HASH} \
   -t $SERVER_IMAGE_NAME:$APP_TAG \
+  -t $SERVER_IMAGE_NAME:$DOCKER_CACHE_TAG \
   --cache-from $SERVER_IMAGE_NAME:$DOCKER_CACHE_TAG \
+  --cache-to=type=inline \
   -f services/fin/Dockerfile \
   .
-docker tag $SERVER_IMAGE_NAME:$APP_TAG $SERVER_IMAGE_NAME:$DOCKER_CACHE_TAG
 
 # Core Server - elastic search
-docker build \
+$DOCKER_BUILD \
   --build-arg FIN_APP_VERSION=${APP_VERSION} \
   --build-arg FIN_REPO_TAG=${FIN_TAG_NAME} \
   --build-arg FIN_BRANCH_NAME=${FIN_BRANCH_NAME} \
   --build-arg FIN_SERVER_REPO_HASH=${FIN_SERVER_REPO_HASH} \
   -t $ELASTIC_SEARCH_IMAGE_NAME:$APP_TAG \
+  -t $ELASTIC_SEARCH_IMAGE_NAME:$DOCKER_CACHE_TAG \
   --cache-from $ELASTIC_SEARCH_IMAGE_NAME:$DOCKER_CACHE_TAG \
+  --cache-to=type=inline \
   services/elastic-search
-docker tag $ELASTIC_SEARCH_IMAGE_NAME:$APP_TAG $ELASTIC_SEARCH_IMAGE_NAME:$DOCKER_CACHE_TAG
 
-# Core Server - elastic search
-docker build \
+# Core Server - rabbitmq
+$DOCKER_BUILD \
   --build-arg FIN_APP_VERSION=${APP_VERSION} \
   --build-arg FIN_REPO_TAG=${FIN_TAG_NAME} \
   --build-arg FIN_BRANCH_NAME=${FIN_BRANCH_NAME} \
   --build-arg FIN_SERVER_REPO_HASH=${FIN_SERVER_REPO_HASH} \
   -t $RABBITMQ_IMAGE_NAME:$APP_TAG \
+  -t $RABBITMQ_IMAGE_NAME:$DOCKER_CACHE_TAG \
   --cache-from $RABBITMQ_IMAGE_NAME:$DOCKER_CACHE_TAG \
+  --cache-to=type=inline \
   services/rabbitmq
-docker tag $RABBITMQ_IMAGE_NAME:$APP_TAG $RABBITMQ_IMAGE_NAME:$DOCKER_CACHE_TAG
 
 # Core - Init services
-docker build \
+$DOCKER_BUILD \
   --build-arg FIN_SERVER_IMAGE=${SERVER_IMAGE_NAME}:${APP_TAG} \
   --build-arg FIN_APP_VERSION=${APP_VERSION} \
   --build-arg FIN_REPO_TAG=${FIN_TAG_NAME} \
   --build-arg FIN_BRANCH_NAME=${FIN_BRANCH_NAME} \
   --build-arg FIN_SERVER_REPO_HASH=${FIN_SERVER_REPO_HASH} \
   -t $INIT_IMAGE_NAME:$APP_TAG \
+  -t $INIT_IMAGE_NAME:$DOCKER_CACHE_TAG \
   --cache-from $INIT_IMAGE_NAME:$DOCKER_CACHE_TAG \
+  --cache-to=type=inline \
   services/init
-docker tag $INIT_IMAGE_NAME:$APP_TAG $INIT_IMAGE_NAME:$DOCKER_CACHE_TAG
 
 # Core - PG REST
-docker build \
+$DOCKER_BUILD \
   --build-arg FIN_APP_VERSION=${APP_VERSION} \
   --build-arg FIN_REPO_TAG=${FIN_TAG_NAME} \
   --build-arg FIN_BRANCH_NAME=${FIN_BRANCH_NAME} \
   --build-arg FIN_SERVER_REPO_HASH=${FIN_SERVER_REPO_HASH} \
   -t $PGREST_IMAGE_NAME:$APP_TAG \
+  -t $PGREST_IMAGE_NAME:$DOCKER_CACHE_TAG \
   --cache-from $PGREST_IMAGE_NAME:$DOCKER_CACHE_TAG \
+  --cache-to=type=inline \
   services/pg-rest
-docker tag $PGREST_IMAGE_NAME:$APP_TAG $PGREST_IMAGE_NAME:$DOCKER_CACHE_TAG
--- devops/submit.sh
+++ devops/submit.sh
@@ -10,5 +10,6 @@ gcloud config set project $GC_PROJECT_ID
 echo "Submitting build to Google Cloud..."
 gcloud builds submit \
   --config ./devops/cloudbuild.yaml \
+  --region us-central1 \
   --substitutions=REPO_NAME=fin,TAG_NAME=$(git describe --tags --abbrev=0),BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD),SHORT_SHA=$(git log -1 --pretty=%h) \
   .
