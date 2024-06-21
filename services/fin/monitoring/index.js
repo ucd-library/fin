6468927e2487dab4f6145356159ed8a535f2a47e
Justin Merz <jrmerz@gmail.com>
2024-06-21 08:03:55 -0700

    update for metrics harvesting

--- services/fin/monitoring/index.js
+++ services/fin/monitoring/index.js
@@ -1,39 +1,20 @@
-const {logger, config, metrics} = require('@ucd-lib/fin-service-utils');
+const {logger, config} = require('@ucd-lib/fin-service-utils');
 
 if( !config.metrics.enabled ) {
   logger.info('metrics are disabled');
   return;
 }
 
+logger.info('enabling metrics', config.metrics);
+
 if( config.metrics.instruments.fs.enabled ) {
-  logger.info('fs metrics enabled');
   require('./lib/fs-test.js');
 }
 
 if( config.metrics.instruments.fcrepo.enabled ) {
-  logger.info('fcrepo metrics enabled');
   require('./lib/fcrepo.js');
 }
 
 if( config.metrics.instruments.fin.enabled ) {
-  logger.info('fin metrics enabled');
   require('./lib/fin.js');
 }
-
-// function harvestLoop() {
-//   try {
-//     logger.info('harvesting');
-//   } catch (e) {
-//     logger.error(e);
-//   }
-
-//   setTimeout(harvestLoop, config.metrics.harvestInterval);
-// }
-
-// if( !config.metrics.enabled ) {
-//   logger.info('metrics harvester disabled');
-//   return;
-// }
-
-// logger.info('starting metrics harvester');
-// harvestLoop();
